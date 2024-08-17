7f5c618f86b26f761a1434023f2500259913e12c
klsierraj <klsierraj47@gmail.com>
2024-08-16 20:51:07 -0500

    filter by price

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,50 +1,61 @@
 class ProductsController < ApplicationController
-
-def index
-  @categories = Category.order(name: :asc)
-  @products = Product.all.with_attached_photo.order(created_at: :desc)
-  if params[:category_id]
-    @products = @products.where(category_id: params[:category_id])
+  def index
+    @categories = Category.order(name: :asc)
+    @products = Product.with_attached_photo.order(created_at: :desc)
+    if params[:category_id]
+      @products = @products.where(category_id: params[:category_id])
+    end
+    if params[:min_price].present?
+      @products = @products.where("price >= ?", params[:min_price])
+    end
+    if params[:max_price].present?
+      @products = @products.where("price <= ?", params[:max_price])
+    end
   end
-end
-def show
-product
-end
-def new
-@product = Product.new
-end
-def create
-  @product = Product.new(product_params)
 
-  if @product.save
-    redirect_to products_path, notice: t('.created')
-  else
-    render :new, status: :unprocessable_entity
+  def show
+    product
   end
-end
-def edit
- product
-end
-def update
-  if product.update(product_params)
-    redirect_to products_path, notice: t('.updated')
-  else
-    render :edit, status: :unprocessable_entity
+
+  def new
+    @product = Product.new
   end
-end
-def destroy
-  product.destroy
-  redirect_to products_path, notice: t('.destroyed'), status: :see_other
-end
 
-private
+  def create
+    @product = Product.new(product_params)
 
-def product_params
-  params.require(:product).permit(:title, :description, :price, :photo, :category_id)
-end
+    if @product.save
+      redirect_to products_path, notice: t('.created')
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
-def product
-  @product = Product.find(params[:id])
-end
+  def edit
+    product
+  end
 
+  def update
+    if product.update(product_params)
+      redirect_to products_path, notice: t('.updated')
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    product.destroy
+
+    redirect_to products_path, notice: t('.destroyed'), status: :see_other
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :description, :price, :photo, :category_id)
+  end
+
+  def product
+    @product = Product.find(params[:id])
+  end
 end
